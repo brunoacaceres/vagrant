995c3b7ec5756febb15a845a0b864238215a6d4f
Bruno A. Caceres <brunocaceres@hotmail.com>
2016-03-14 07:54:52 -0400

    Atualizando criacao nginx + mysql

---
+++ testevm/dockerapp/nginx/docker_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DockerExample::Application.config.secret_token = '6f4128b6fc5f1b30489cc0fd8ed14c1886ea5a4cc6265b58c9412dc6fb429b3c1d9985c3a055775907e316d0938f027388b098ba32112eea5b17b6d09d417aa1'
